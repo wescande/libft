0bd24007ebb5337d97b2360fad508860685bac3c
wescande <wescande@student.42.fr>
2016-11-17 17:10:50 +0100

    modif script srcs files

--- addsrcs.sh
+++ addsrcs.sh
@@ -1,3 +1,1 @@
-#ls srcs/*/* | sed -e "s/srcs\///" -e 's/$/\\/'
 expr "$(ls srcs/*/* | sed -e "s/srcs\///" -e 's/$/\\/')" : "\(.*\).$"
-#expr "$((ls srcs/*/* | sed -e "s/srcs\///" | awk 'length($1)>15{print}' | sed 's/$/\\/' | tr "\\" "\t" | sed 's/$/\\/' && ls srcs/*/* | sed -e "s/srcs\///" | awk 'length($1)<16{print}' | sed 's/$/\\/' | tr "\\" "\t" | sed 's/$/\\/' | tr "\\" "\t" | sed 's/$/\\/') | sort -fd)" : "\(.*\)..$"
