cbe6189be96157a7ebce04c32d9f9481bd933964
william <william.escande@gmail.com>
2016-12-26 03:14:32 +0100

    ld swap

--- srcs/ld/ft_ld_swap.c
+++ srcs/ld/ft_ld_swap.c
@@ -6,7 +6,7 @@
 /*   By: wescande <wescande@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/24 21:12:53 by wescande          #+#    #+#             */
-/*   Updated: 2016/12/24 21:23:17 by wescande         ###   ########.fr       */
+/*   Updated: 2016/12/24 23:52:38 by wescande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ t_ld		*ft_ld_swap(t_ld *l_cur)
 		l_prev = l_cur->prev;
 		l_next = l_cur->next;
 		l_cur->next = l_next->next;
+		if (l_cur->next)
+			l_cur->next->prev = l_cur;
 		l_cur->prev = l_next;
 		l_next->next = l_cur;
 		l_next->prev = l_prev;
